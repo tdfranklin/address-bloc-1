b89ee92e653d9144f3b61c8b32362ca99c1b4f1f
Tyler D. Franklin <irishlep84@gmail.com>
2017-12-20 14:49:51 -0600

    Checkpoint 3 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,15 +5,8 @@ require 'bloc_record/base'
 class AddressBook < BlocRecord::Base
   attr_reader :entries
 
-  def add_entry(name, phone_number, email)
-    index = 0
-    entries.each do |entry|
-      if name < entry.name
-        break
-      end
-      index += 1
-    end
-    entries.insert(index, Entry.new(name, phone_number, email))
+  def add_entry(name, phone, email)
+    Entry.create(name: name, phone_number: phone, email: email, address_book_id: self.id)
   end
 
   def import_from_csv(file_name)
@@ -24,26 +17,5 @@ class AddressBook < BlocRecord::Base
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-  end
-
-  # Search AddressBook for a specific entry by name
-  def binary_search(name)
-    lower = 0
-    upper = entries.length - 1
-
-    while lower <= upper
-      mid = (lower + upper) / 2
-      mid_name = entries[mid].name
-
-      if name == mid_name
-        return entries[mid]
-      elsif name < mid_name
-        upper = mid - 1
-      elsif name > mid_name
-        lower = mid + 1
-      end
-    end
-
-    return nil
   end
 end
